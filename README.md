4c08766f818d307eb9a384d0fbb14073d351d54f
Николай Митин <nick.mitin@gmail.com>
2020-11-29 22:15:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# php-emarsys-connector
+
+## Install
+
+``` sh
+$ composer require qortex/php-emarsys-connector
+```
+
+## Use
+
+First, obtain credentials for [Emarsys API User](https://help.emarsys.com/hc/en-us/articles/115004740329-your-account-security-settings#api-users).
+
+Then, pass these credentials as username and secret to `EmarsysConnector` constructor:
+``` php
+use Qortex\Emarsys\Connector as EmarsysConnector;
+
+$emarsysConnector = new EmarsysConnector($username, $secret);
+```
+Last, use any of the following `EmarsysConnector` methods to communicate with Emarsys Omnichannel Customer Engagement Platform:
+
+``` php
+function queryContacts(string $key, string $value)
+```
+Queries all contacts matching `$key` with the `$value` in Emarsys contacts database, regardless the contact lists.
+
+Emarsys API [List Contact Data](https://dev.emarsys.com/v2/contacts/list-contact-data)
+
+``` php
+function createContact(string $key, array $properties)
+```
+Creates a contact in Emarsys contacts database and populates its properties with `$properties` . `$key` is used to provide uniqueness of the contact.
+
+Emarsys API [Create Contacts](https://dev.emarsys.com/v2/contacts/create-contacts)
+
+``` php
+function deleteContact(string $key, string $value)
+```
+Deletes a contact with `$key` equals `$value` from Emarsys contacts database.
+
+Emarsys API [Delete a Contact](https://dev.emarsys.com/v2/contacts/delete-contact)
+
+``` php
+function addContactToContactListById(int $listId, int $contactId)
+```
+
+Adds a contact defined by `$contactId` to a contact list defined by `$listId`.
+
+Emarsys API [Add Contacts to a Contact List](https://dev.emarsys.com/v2/contact-lists/add-contacts-to-a-contact-list)
+
+``` php
+function removeContactFromContactListById(int $listId, int $contactId)
+```
+Removes a contact defined by `$contactId` from a contact list defined by `$listId`. 
+
+Emarsys API [Remove Contacts from a Contact List](https://dev.emarsys.com/v2/contact-lists/remove-contacts-from-a-contact-list)
+
+``` php
+function countContactsInAContactList(int $listId)
+```
+Counts contacts in a contact list defined by `$listId`
+
+Emarsys API [Count Contacts in a Contact List](https://dev.emarsys.com/v2/contact-lists/count-contacts-in-a-contact-list)
---
+++ src/Services/Connector.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Qortex\Emarsys;
+
+use GuzzleHttp\Client;
+
+class Connector
+{
+    private string $username;
+    private string $secret;
+    private string $apiUrl;
+
+    public function __construct($username, $secret, $apiUrl = null)
+    {
+        $this->username = $username;
+        $this->secret = $secret;
+        $this->apiUrl = $apiUrl ?? 'https://api.emarsys.net/api/v2/';
+    }
+
+    private function prepareRequest($endpoint)
+    {
+        $timestamp = gmdate('c');
+        $client = new Client([
+            'base_uri' => $this->apiUrl,
+            'headers' => [
+                #X-WSSE header description https://dev.emarsys.com/v2/before-you-start/authentication
+                'X-WSSE' => 'UsernameToken Username="' . $this->username .
+                    '", PasswordDigest="' . $this->getPasswordDigest($timestamp) .
+                    '", Nonce="' . md5(uniqid()) .
+                    '", Created="' . $timestamp . '"',
+                'Content-type' => 'application/json;charset=utf-8',
+            ]
+        ]);
+        return $client;
+    }
+
+    private function sendPostRequest($endpoint, $payload)
+    {
+        $client = $this->prepareRequest($endpoint);
+        $response = $client->request('POST', $endpoint . '/', ['json' => $payload]);
+        return json_decode($response->getBody()->getContents());
+    }
+
+    private function sendGetRequest($endpoint, $query = [])
+    {
+        $client = $this->prepareRequest($endpoint);
+        $response = $client->request('GET', $endpoint . '/', ['query' => $query]);
+        return json_decode($response->getBody()->getContents());
+    }
+
+    private function getPasswordDigest($timestamp)
+    {
+        #Password digest algorithm description https://dev.emarsys.com/v2/first-steps/configure-authentication
+        return base64_encode(sha1($this->nonce . $timestamp . $this->secret, false));
+    }
+
+    public function queryContacts(string $key, string $value)
+    {
+        return $this->sendGetRequest('contact/query', [
+            $key => $value,
+            'return' => $key
+        ]);
+    }
+
+    public function createContact(string $key, array $properties)
+    {
+        return $this->sendPostRequest('contact', [
+            'key_id' => $key,
+            'contacts' => [
+                $properties
+            ],
+        ]);
+    }
+
+    public function deleteContact(string $key, string $value)
+    {
+        return $this->sendPostRequest('contact/delete', [
+            'key_id' => $key,
+            $key => $value,
+        ]);
+    }
+
+    public function removeContactFromContactListById(int $listId, int $contactId)
+    {
+        return $this->sendPostRequest('contactlist/' . $listId . '/delete', [
+            'key_id' => 'id',
+            'external_ids' => [
+                $contactId
+            ],
+        ]);
+    }
+
+    public function addContactToContactListById(int $listId, int $contactId)
+    {
+        return $this->sendPostRequest('contactlist/' . $listId . '/add', [
+            'key_id' => 'id',
+            'external_ids' => [
+                $contactId
+            ],
+        ]);
+    }
+
+    public function countContactsInAContactList(int $listId)
+    {
+        return $this->sendGetRequest('contactlist/' . $listId . '/count');
+    }
+}
